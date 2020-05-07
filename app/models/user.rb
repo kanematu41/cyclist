c4e568cce62f9731e2b825547195f172129103c5
unknown <grenson.crtis@gmail.com>
2020-05-07 21:48:59 +0900

    [add]コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   attachment :image #refileでの画像アップ用
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
 end
