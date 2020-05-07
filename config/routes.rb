6a911edc613101131377bcec92962c0242d2400c
unknown <grenson.crtis@gmail.com>
2020-05-07 20:26:36 +0900

    [add]ユーザー編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 	root 'home#top'
   devise_for :users
+  resources :users, only:[:show, :edit, :update]
   resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
