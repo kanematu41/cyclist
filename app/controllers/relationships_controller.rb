1e1bbbfa259ce5846ba2c8d4874c2624c35cc6a0
unknown <grenson.crtis@gmail.com>
2020-05-09 18:04:37 +0900

    [add]フォロー非同期機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,17 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
 	def create
+		@user = User.find(params[:id])
 		current_user.follow(params[:id])
 		# フォロー通知
-		@user = User.find(params[:id])
 		@user.create_notification_follow!(current_user)
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
+		@user = User.find(params[:id])
 		current_user.unfollow(params[:id])
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 
 	def follows
