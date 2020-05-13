647afd7cf813735d8cd0bd21fbd933cab87b555d
unknown <grenson.crtis@gmail.com>
2020-05-13 19:28:44 +0900

    deploy setting s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-# CarrierWave.configure do |config|
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#       region: ENV['AWS_REGION'],
-#     }
-#     config.fog_directory  = 's3-cyclist-image'
-# end
+CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+    }
+    config.fog_directory  = 's3-cyclist-image'
+end
 
-# CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
