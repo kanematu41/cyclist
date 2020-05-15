adbb03739f2e170bf011ebe7aa5d87ff6e11a370
unknown <grenson.crtis@gmail.com>
2020-05-15 15:18:22 +0900

    [update]view修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,13 +15,4 @@ class RelationshipsController < ApplicationController
 		# redirect_back(fallback_location: root_path)
 	end
 
-	def follows
-		@user = User.find(params[:id])
-		@follows = @user.following_user
-	end
-
-	def followers
-		@user = User.find(params[:id])
-		@followers = @user.follower_user
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,26 @@ Rails.application.routes.draw do
 
 	root 'home#top'
   devise_for :users
-  resources :users, only:[:show, :edit, :update]
+
+  resources :users, only:[:show, :edit, :update] do
+    member do
+      get :follows
+      get :followers
+      get :timeline
+    end
+  end
+
   resources :posts do
   	resource :comments, only:[:create, :destroy]
   	resource :likes, only:[:create, :destroy]
   end
+
   resources :notifications, only:[:index, :destroy]
   resources :searchs, only:[:index]
   resources :chats, only: [:show, :create]
 
   post 'follow/:id' => 'relationships#create', as: 'follow'
   delete 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
-  get 'user/:id/follows' => 'relationships#follows', as: 'follows'
-  get 'user/:id/followers' => 'relationships#followers', as: 'followers'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
