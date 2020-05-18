abb7d7919a59693b716a268a8a95752a0e00e3d3
unknown <grenson.crtis@gmail.com>
2020-05-18 20:52:49 +0900

    [update]管理者機能

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -14,6 +14,7 @@ class SearchsController < ApplicationController
 
   def tag
     if params[:tag_name]
+      @tag_name = params[:tag_name]
       @posts = Post.page(params[:page]).reverse_order.tagged_with("#{params[:tag_name]}")
     end
   end
